0d22199dc7afd82feaf297a1498b6c2ddcf158f7
temkinman <temkinman@mail.ru>
2020-09-27 19:32:50 +0300

    add undo feature

--- js/myscript.js
+++ js/myscript.js
@@ -11,13 +11,35 @@ let state = {
     { id: getId(), title: "learning React", isComplete: false },
   ],
   todoEdited: null,
+  undoStack: [],
 };
 
 const setState = (newStatePart) => {
   state = { ...state, ...newStatePart };
+  const {undoStack, ...allState} = newStatePart;
+  state.undoStack.push(allState);
+
   const newHtml = render(state);
   renderToDom(newHtml);
 };
+
+const setInitialState = () => {
+  state.undoStack.push(state);
+}
+
+setInitialState();
+
+const setStateUndo = () => {
+  state.undoStack.pop();
+  const newUndoStack = state.undoStack[state.undoStack.length - 1];
+  state.todos = newUndoStack.todos;
+  // {state, newUndoStack}
+  
+  // const newState = {...state, newUndoStack};
+  // setState(newState);
+  const newHtml = render(newUndoStack);
+  renderToDom(newHtml); 
+}
 
 const getState = () => {
   return state;
@@ -46,7 +68,7 @@ const getFormData = (formElement) => {
 const onAddNewTodo = (formElement, event) => {
   event.preventDefault();
   const formData = getFormData(formElement);
-  if( formData.title.length === 0) return;
+  if (formData.title.length === 0) return;
   const newTodo = {
     id: getId(),
     title: formData.title,
@@ -86,34 +108,55 @@ const updateStatus = (todos, todoId) =>
 const removeTodo = (todoId) => state.todos.filter((todo) => todo.id !== todoId);
 
 const onShowActiveTodos = () => {
-  const activeTodosHtml = render({todos: filterStatusTodos(state.todos, false)});
+  const activeTodosHtml = render({
+    todos: filterStatusTodos(state.todos, false),
+  });
   renderToDom(activeTodosHtml);
-  document.getElementById('active').classList.add('active');
+  document.getElementById("active").classList.add("active");
   // renderToDom('');
   // setState();
-}
+};
 
 const onShowCompletedTodos = () => {
-  const activeTodosHtml = render({todos: filterStatusTodos(state.todos, true)});
-  renderToDom(activeTodosHtml);
-  document.getElementById('complete').classList.add('active');
+  const completedTodosHtml = render({
+    todos: filterStatusTodos(state.todos, true),
+  });
+  renderToDom(completedTodosHtml);
+  document.getElementById("complete").classList.add("active");
   // renderToDom('');
   // setState({todos: filterStatusTodos(state.todos, true)});
-}
+};
 
 const onShowAllTodos = () => {
   setState(state);
-  document.getElementById('all').classList.add('active');
-}
+  document.getElementById("all").classList.add("active");
+};
 
-const filterStatusTodos = (todos, status) => todos.filter(({isComplete}) => isComplete === status);
+const filterStatusTodos = (todos, status) =>
+  todos.filter(({ isComplete }) => isComplete === status);
+
+const countActiveTodos = (todos) => {
+  return filterStatusTodos(todos, false).length;
+};
+
+const undoStack = () => {
+  const newUndostack = state.undoStack[state.undoStack.length - 1];
+  console.log('undostack in function');
+  console.log(newUndostack);
+  // state.undoStack.pop();
+  console.log('-------------');
+  console.log({...newUndostack});
+
+  setState({...state, ...newUndostack});
+}
 
 const renderToDom = (template) => {
   document.getElementById("app").innerHTML = template;
 };
 
-const render = ({ todos, todoEdited }) =>
-  `
+const render = ({ todos, todoEdited }) => {
+  const activeTodos = countActiveTodos(state.todos);
+  return  `
   <div class="top-todo">
     <h1 class="name-app">Todo list</h1>
     <form class="add-todo" onsubmit="onAddNewTodo(this, event)">
@@ -139,7 +182,9 @@ const render = ({ todos, todoEdited }) =>
           todo.id == todoEdited
             ? `
         <li class="todo-added" id="${todo.id}">
-          <form onsubmit="onSaveTitle(this, event, ${todo.id})" class="formEdit">  
+          <form onsubmit="onSaveTitle(this, event, ${
+            todo.id
+          })" class="formEdit">  
             <input type="checkbox" 
                 class="complete" ${todo.isComplete ? "checked" : ""}
                 name="isComplete"
@@ -169,7 +214,9 @@ const render = ({ todos, todoEdited }) =>
                 todo.title
               }</span>
           <div class="icons">
-            <img src="./images/pencil.png" alt="" class="edit" onclick="onEdit(${todo.id})">
+            <img src="./images/pencil.png" alt="" class="edit" onclick="onEdit(${
+              todo.id
+            })">
             <img src="./images/trash-alt-solid.svg" alt="" class="delete" onclick="onRemoveTodo(${
               todo.id
             })"/>
@@ -185,7 +232,13 @@ const render = ({ todos, todoEdited }) =>
     <button class="add-button mr-5" id="complete" onclick="onShowCompletedTodos()">completed</button>
     <button class="add-button mr-5" id="all" onclick="onShowAllTodos()">all</button>
   </div>
+  <div>
+    <button onclick="">redo</button>
+    <button onclick="setStateUndo()">undo</button>
+  </div>
+  <span class="active-todos">active todos: ${activeTodos}</span>
 `;
+};
 
 const main = () => {
   document.getElementById("app").innerHTML = render(state);
