bbbfd9a7a37939fa3716d665ebe4ea1aa38308c1
Chegodar Artem <temkinman@mail.ru>
2020-09-29 18:51:48 +0300

    fixed bugs with history and filters, added some styles

--- js/myscript.js
+++ js/myscript.js
@@ -16,6 +16,7 @@ let state = {
     allStates: [],
     cursor: -1,
   },
+  filter: "all",
 };
 
 //historyFlag означает если вы сетаете state из ф-ции redoHistory или undoHistory, то измененый state не будет ложится заново в history
@@ -26,16 +27,24 @@ const setState = (newStatePart, historyFlag = false) => {
     state.history.allStates = addHistory(state);
     state.history.cursor = state.history.allStates.length - 1;
   }
-
-  const newHtml = render(state);
-  renderToDom(newHtml);
+  renderWithFilter(state);
 };
+
+const renderWithFilter = (state) => {
+  if (state.filter === "active" || state.filter === "completed") {
+    const stateWithFilter = filterStatusTodos(state, state.filter);
+    const newHtml = render(stateWithFilter);
+    renderToDom(newHtml);
+  } else {
+    const newHtml = render(state);
+    renderToDom(newHtml);
+  }
+}
 
 const addHistory = ({ history, todos }) => [...history.allStates, todos];
 
-const undoHistory = ({history}) => {
+const undoHistory = ({ history }) => {
   const historyFlag = true;
-  let newState = null;
 
   if (history.cursor === 0) {
     history.cursor -= 1;
@@ -44,19 +53,18 @@ const undoHistory = ({history}) => {
 
   if (history.cursor !== -1) {
     history.cursor -= 1;
-    newState = history.allStates[history.cursor];
-    setState({todos:newState}, historyFlag);
+    const newState = history.allStates[history.cursor];
+    setState({ todos: newState }, historyFlag);
   }
 };
 
-const redoHistory = ({history}) => {
+const redoHistory = ({ history }) => {
   const historyFlag = true;
-  let newState = null;
 
   if (history.cursor < history.allStates.length - 1) {
     history.cursor += 1;
-    newState = history.allStates[history.cursor];
-    setState({todos:newState}, historyFlag);
+    const newState = history.allStates[history.cursor];
+    setState({ todos: newState }, historyFlag);
   }
 };
 
@@ -87,12 +95,15 @@ const getFormData = (formElement) => {
 const onAddNewTodo = (formElement, event) => {
   event.preventDefault();
   const formData = getFormData(formElement);
+
   if (formData.title.length === 0) return;
+
   const newTodo = {
     id: getId(),
     title: formData.title,
     isComplete: formData.isComplete === "on",
   };
+
   setState({
     todos: addTodo(state.todos, newTodo),
   });
@@ -112,9 +123,7 @@ const onChangeStatus = (todoId) => {
   });
 };
 
-const addTodo = (todos, newTodo) => {
-  return [...todos, newTodo];
-};
+const addTodo = (todos, newTodo) => [...todos, newTodo];
 
 const updateTitle = (todos, todoId, title) =>
   todos.map((todo) => (todo.id === todoId ? { ...todo, title } : todo));
@@ -126,43 +135,54 @@ const updateStatus = (todos, todoId) =>
 
 const removeTodo = (todoId) => state.todos.filter((todo) => todo.id !== todoId);
 
-const onShowActiveTodos = (state) => {
-  const activeTodosHtml = render({
-    todos: filterStatusTodos(state.todos, false),
-  });
-  renderToDom(activeTodosHtml);
-  document.getElementById("active").classList.add("active");
+const onShowActiveTodos = () => {
+  setFilter("active");
 };
 
-const onShowCompletedTodos = (state) => {
-  const completedTodosHtml = render({
-    todos: filterStatusTodos(state, true),
-  });
-  renderToDom(completedTodosHtml);
-  document.getElementById("complete").classList.add("active");
+const onShowCompletedTodos = () => {
+  setFilter("completed");
 };
 
-const onShowAllTodos = (state) => {
-  const allTodosHtml = render(state);
-  renderToDom(allTodosHtml);
-  document.getElementById("all").classList.add("active");
+const onShowAllTodos = () => {
+  setFilter("all");
 };
 
-const filterStatusTodos = (todos, status) =>
-  todos.filter(({ isComplete }) => isComplete === status);
+const setFilter = (filter) => {
+  setState({ filter }, true); //true it's for not to save in the history
+};
 
-const countActiveTodos = (todos) => {
-  return filterStatusTodos(todos, false).length;
+const filterStatusTodos = ({ todos, filter }) => {
+  if (filter === "all") return state;
+  
+  const isActive = filter === "active" ? false : true;
+  
+  const filteredTodos = todos.filter(
+    ({ isComplete }) => isComplete === isActive
+  );
+
+  return (newTodos = { ...state, todos: filteredTodos });
+};
+
+const countActiveTodos = () => {
+  const notFilteredState = getState();
+  return notFilteredState.todos.filter(({ isComplete }) => isComplete === false).length;
 };
 
 const renderToDom = (template) => {
   document.getElementById("app").innerHTML = template;
 };
 
-const render = ({ todos, todoEdited = null, history }) => {
-  const activeTodos = countActiveTodos(state.todos);
-  const undoEnabled = ( history.allStates.length === 0 || todos.length === 0 ) ? "disabled" : "";
-  const redoEnabled = ( history.allStates.length === 0 || history.cursor === history.allStates.length - 1 )
+const render = (state) => {
+  const { todos, history, todoEdited, filter } = { ...state };
+  const activeTodos = countActiveTodos(todos);
+  const activeFilter = filter === "active" ? "active" : "";
+  const completedFilter = filter === "completed" ? "active" : "";
+  const allFilter = filter === "all" ? "active" : "";
+  const undoEnabled =
+    history.allStates.length === 0 || todos.length === 0 ? "disabled" : "";
+  const redoEnabled =
+    history.allStates.length === 0 ||
+    history.cursor === history.allStates.length - 1
       ? "disabled"
       : "";
 
@@ -170,19 +190,27 @@ const render = ({ todos, todoEdited = null, history }) => {
   <div class="top-todo">
     <h1 class="name-app">Todo list</h1>
     <form class="add-todo" onsubmit="onAddNewTodo(this, event)">
-      <input type="text"
+      <div class="add-todo-block">  
+        <input type="text"
              class="todo-input"
              placeholder="type todo"
              id="newTodo"
              minlength = 3
              name="title"
              />
-      <input type="checkbox"
-            class="addStatus"
-            name="isComplete"
-      />
-      <span class="addStatus">isComplete</span>
-      <button type="submit" class="add-button" id="add-todo">add</button>
+        <div class="add-todo-status">
+          <input type="checkbox"
+                class="addStatus"
+                name="isComplete"
+          />
+          <span class="addStatus">completed?</span>
+        </div>     
+      </div>  
+      <button type="submit"
+              class="button"
+              id="add-todo">add
+      </button>
+      
     </form>
   </div>
   <div class="todos">
@@ -219,7 +247,7 @@ const render = ({ todos, todoEdited = null, history }) => {
         <li class="todo-added" id="${todo.id}">
           <input type="checkbox"
                 onchange="onChangeStatus(${todo.id})"      
-                style = "color:red"/  ${todo.isComplete ? "checked" : ""}>
+                ${todo.isComplete ? "checked" : ""}/>
           <span class="complete ${todo.isComplete ? "strikeout" : ""}">${
                 todo.title
               }</span>
@@ -238,13 +266,25 @@ const render = ({ todos, todoEdited = null, history }) => {
     </ul>
   </div>
   <div class="show-diff-todos">
-    <button class="add-button mr-5" id="active" onclick="onShowActiveTodos(state)">active</button>
-    <button class="add-button mr-5" id="complete" onclick="onShowCompletedTodos(state)">completed</button>
-    <button class="add-button mr-5" id="all" onclick="onShowAllTodos(state)">all</button>
+    <button class="button mr-5 ${activeFilter}"
+            id="active"
+            onclick="onShowActiveTodos()"
+            >active
+    </button>
+    <button class="button mr-5 ${completedFilter}"
+            id="complete"
+            onclick="onShowCompletedTodos()"
+            >completed
+    </button>
+    <button class="button mr-5 ${allFilter}"
+            id="all"
+            onclick="onShowAllTodos()"
+            >all
+    </button>
   </div>
   <div class="historyBtns">
-    <button onclick="redoHistory(state)" ${redoEnabled} class="mr-5"><img src="./images/redo.png" alt="" class="redo"></button>
-    <button onclick="undoHistory(state)" ${undoEnabled}><img src="./images/undo.png" alt="" class="undo"></button>
+    <button onclick="redoHistory(state)" ${redoEnabled} class="button mr-5"><img src="./images/redo.png" alt="" class="redo"></button>
+    <button onclick="undoHistory(state)" ${undoEnabled} class="button"><img src="./images/undo.png" alt="" class="undo"></button>
   </div>
   <span class="active-todos">active todos: ${activeTodos}</span>
 `;
